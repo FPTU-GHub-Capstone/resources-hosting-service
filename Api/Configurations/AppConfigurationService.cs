8c5569031d0212f80c1be191484189bb9fb76458
danielng <dacnguyen9101@gmail.com>
2023-10-11 01:04:24 +0700

    feat: dockerize mssql

--- Api/Configurations/AppConfigurationService.cs
+++ Api/Configurations/AppConfigurationService.cs
@@ -81,5 +81,13 @@ namespace WebApiLayer.Configurations
                 }
             );
         }
+
+        public static async Task ApplyMigrations(this IServiceProvider serviceProvider)
+        {
+            using var scope = serviceProvider.CreateScope();
+            await using ApplicationDbContext dbContext =
+                scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            await dbContext.Database.MigrateAsync();
+        }
     }
 }
--- Api/Program.cs
+++ Api/Program.cs
@@ -27,6 +27,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    await app.Services.ApplyMigrations();
 }
 
 app.UseAutoWrapper();
