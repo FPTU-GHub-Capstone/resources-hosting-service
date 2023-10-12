c7803bf7d3a7130faea522d2971383d6b86a2672
Hoàng Đình Anh Quốc <anhquoc2906@gmail.com>
2023-10-12 09:05:13 +0700

    Push Game & Game Server Controllers (C,R,U,D)

--- Api/Controllers/GamesController.cs
+++ Api/Controllers/GamesController.cs
@@ -1,35 +1,58 @@
 ﻿using DomainLayer.Constants;
+using DomainLayer.Entities;
 using Microsoft.AspNetCore.Mvc;
+using RepositoryLayer.Repositories;
+using ServiceLayer.Business;
+using WebApiLayer.UserFeatures.Requests;
 
 namespace WebApiLayer.Controllers;
 
 [Route(Constants.HTTP.API_VERSION + "/gms/games")]
 public class GamesController : BaseController
 {
-    [HttpGet]
-    public IEnumerable<string> Get()
+    private readonly IGameServices _gameServices;
+    private readonly IGenericRepository<GameEntity> _gameRepo;
+    public GamesController(IGameServices gameServices, IGenericRepository<GameEntity> gameRepo)
     {
-        return new string[] { "value1", "value2" };
+        _gameServices = gameServices;
+        _gameRepo = gameRepo;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetGames()
+    {
+        return Ok(await _gameServices.List());
     }
 
     [HttpGet("{id}")]
-    public string Get(int id)
+    public async Task<IActionResult> GetGame(Guid id)
     {
-        return "value";
+        var user = await _gameServices.GetById(id);
+        return Ok(user);
     }
 
     [HttpPost]
-    public void Post([FromBody] string value)
+    public async Task<IActionResult> CreateGame([FromBody] CreateGameRequest newGame)
     {
+        var gameEntity = new GameEntity();
+        Mapper.Map(newGame, gameEntity);
+        await _gameServices.Create(gameEntity);
+        return CreatedAtAction("GetGame", new { id = gameEntity.Id }, gameEntity);
     }
 
     [HttpPut("{id}")]
-    public void Put(int id, [FromBody] string value)
+    public async Task<IActionResult> UpdateGame(Guid id, [FromBody] UpdateGameRequest game)
     {
+        var updateGame = await _gameRepo.FindByIdAsync(id);
+        Mapper.Map(game, updateGame);
+        await _gameServices.Update(id, updateGame);
+        return Ok(updateGame);
     }
 
     [HttpDelete("{id}")]
-    public void Delete(int id)
+    public async Task<IActionResult> DeleteGame(Guid id)
     {
+        await _gameServices.Delete(id);
+        return NoContent();
     }
 }
---
+++ Api/UserFeatures/Requests/CreateGameServerRequest.cs
@@ -0,0 +1,18 @@
+﻿using DomainLayer.Entities;
+using System.ComponentModel.DataAnnotations;
+using WebApiLayer.Mappings;
+
+namespace WebApiLayer.UserFeatures.Requests
+{
+    public class CreateGameServerRequest : IMapTo<GameServerEntity>, IMapFrom<GameServerEntity>
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Location { get; set; }
+        [Required]
+        public string ArtifactUrl { get; set; }
+        [Required]
+        public Guid GameId { get; set; }
+    }
+}
