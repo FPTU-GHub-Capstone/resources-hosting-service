b794ffecd409beb18048df5dae9389a724891eab
Nguyễn Đắc <dacnguyen9101@gmail.com>
2023-10-29 17:54:17 +0700

    feat: config console logger for EF

--- Api/Configurations/AppConfigurationService.cs
+++ Api/Configurations/AppConfigurationService.cs
@@ -1,13 +1,12 @@
 ﻿using AutoWrapper;
 using DomainLayer.Constants;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyModel;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Options;
 using RepositoryLayer.Contexts;
 using RepositoryLayer.Repositories;
 using Serilog;
 using Serilog.Events;
-using Serilog.Settings.Configuration;
 using ServiceLayer.Business;
 using ServiceLayer.Core.AppConfig;
 
@@ -42,11 +41,14 @@ namespace WebApiLayer.Configurations
         {
             var settings = services.BuildServiceProvider().GetService<IOptions<AppSettings>>();
             Console.WriteLine(settings);
+            // use Console buildin logger to prevent EF log write to DB stream
             services.AddDbContext<ApplicationDbContext>(
-                options =>
-                    options.UseSqlServer(
-                        settings.Value.ConnectionStrings.DefaultConnection
-                    )
+                options => options
+                    .UseSqlServer(settings.Value.ConnectionStrings.DefaultConnection)
+                    .LogTo(Console.WriteLine,
+                        new[] { DbLoggerCategory.Database.Command.Name },
+                        LogLevel.Information,
+                        DbContextLoggerOptions.SingleLine | DbContextLoggerOptions.UtcTime)
             );
             services.AddScoped<IApplicationDbContext>(
                 provider => provider.GetService<ApplicationDbContext>()
